cdb2ce58d2880d553b9a368214a9907b2dd39a6d
Thais Laure Silva Magalhães <thais.magalhaes@ewave.com.br>
2021-06-15 11:55:35 -0400

    Add project files.

---
+++ Api.Application/Startup.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using Api.CrossCutting.DependencyInjection;
+using Api.Domain.Security;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+
+namespace Application {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices (IServiceCollection services) {
+
+            ConfigureService.ConfigureDependenciesService (services);
+            ConfigureRepository.ConfigureDependenciesRepository (services);
+
+            services.AddSwaggerGen (c => {
+                c.SwaggerDoc ("v1", new OpenApiInfo {
+                    Title = "ASPNETCORE 3.1 - 2020",
+                        Version = "v1",
+                        Description = "Exemplo de API REST criada no curso",
+                        Contact = new OpenApiContact {
+                            Name = "Marcos Fabricio Rosa",
+                                Email = string.Empty,
+                                Url = new Uri ("https://github.com/mfrinfo"),
+                        },
+                });
+
+                //Colocar JWT no Swagger
+                c.AddSecurityDefinition ("Bearer", new OpenApiSecurityScheme {
+                    Description = "Entre com o Token JWT",
+                        Name = "Authorization",
+                        In = ParameterLocation.Header,
+                        Type = SecuritySchemeType.ApiKey
+                });
+
+                c.AddSecurityRequirement (new OpenApiSecurityRequirement {
+                    {
+                        new OpenApiSecurityScheme {
+                            Reference = new OpenApiReference {
+                                Id = "Bearer",
+                                    Type = ReferenceType.SecurityScheme
+                            }
+                        }, new List<string> ()
+                    }
+                });
+            });
+
+            var signingConfigurations = new SigningConfigurations ();
+            services.AddSingleton (signingConfigurations); //Instancia Unica
+
+            var tokenConfigurations = new TokenConfigurations ();
+            new ConfigureFromConfigurationOptions<TokenConfigurations> (
+                    Configuration.GetSection ("TokenConfigurations"))
+                .Configure (tokenConfigurations);
+            services.AddSingleton (tokenConfigurations); //Instancia Unica
+
+            services.AddAuthentication (authOptions => {
+                authOptions.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                authOptions.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer (bearerOptions => {
+                var paramsValidation = bearerOptions.TokenValidationParameters;
+                paramsValidation.IssuerSigningKey = signingConfigurations.Key;
+                paramsValidation.ValidAudience = tokenConfigurations.Audience;
+                paramsValidation.ValidIssuer = tokenConfigurations.Issuer;
+
+                // Valida a assinatura de um token recebido
+                paramsValidation.ValidateIssuerSigningKey = true;
+
+                // Verifica se um token recebido ainda é válido
+                paramsValidation.ValidateLifetime = true;
+
+                // Tempo de tolerância para a expiração de um token (utilizado
+                // caso haja problemas de sincronismo de horário entre diferentes
+                // computadores envolvidos no processo de comunicação)
+                paramsValidation.ClockSkew = TimeSpan.Zero;
+            });
+
+            // Ativa o uso do token como forma de autorizar o acesso
+            // a recursos deste projeto
+            services.AddAuthorization (auth => {
+                auth.AddPolicy ("Bearer", new AuthorizationPolicyBuilder ()
+                    .AddAuthenticationSchemes (JwtBearerDefaults.AuthenticationScheme‌​)
+                    .RequireAuthenticatedUser ().Build ());
+            });
+
+            // services.AddMvc(Options => { Options.EnableEndpointRouting = false; })
+            //         .AddNewtonsoftJson();
+            services.AddControllers ()
+                .AddNewtonsoftJson ();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
+            }
+
+            // Ativando middlewares para uso do Swagger
+            app.UseSwagger ();
+            app.UseSwaggerUI (c => {
+                c.RoutePrefix = string.Empty;
+                c.SwaggerEndpoint ("/swagger/v1/swagger.json", "Curso de API com AspNetCore 3.0");
+            });
+
+            // Redireciona o Link para o Swagger, quando acessar a rota principal
+            var option = new RewriteOptions ();
+            option.AddRedirect ("^$", "swagger");
+            app.UseRewriter (option);
+            // app.UseMvc();
+            app.UseRouting ();
+            app.UseAuthorization ();
+            app.UseEndpoints (endpoints => {
+                endpoints.MapControllers ();
+            });
+        }
+    }
+}
